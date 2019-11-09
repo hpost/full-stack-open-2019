84fad2ffd3282818c1e3205420949d3a0a306f61
Henning Post <henningpost@gmail.com>
2019-11-08 16:45:04 -0800

    unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,14 +13,23 @@ const FeedbackButtons = ({ handleGood, handleNeutral, handleBad }) => (
   </div>
 )
 
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-)
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all || 0
+  const positive = good / all || 0
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
