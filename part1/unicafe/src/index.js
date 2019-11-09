8692b931b9f6b2b20493bb06581f5994f625bd36
Henning Post <henningpost@gmail.com>
2019-11-08 16:53:00 -0800

    unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,6 +13,10 @@ const FeedbackButtons = ({ handleGood, handleNeutral, handleBad }) => (
   </div>
 )
 
+const Statistic = ({ text, value }) => (
+  <p>{text} {value}</p>
+)
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   const average = (good - bad) / all || 0
@@ -23,12 +27,12 @@ const Statistics = ({ good, neutral, bad }) => {
       <h1>statistics</h1>
       {all > 0 &&
         <>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {all}</p>
-          <p>average {average}</p>
-          <p>positive {positive} %</p>
+          <Statistic text='good' value={good} />
+          <Statistic text='neutral' value={neutral} />
+          <Statistic text='bad' value={bad} />
+          <Statistic text='all' value={all} />
+          <Statistic text='average' value={average} />
+          <Statistic text='positive' value={`${positive} %`} />
         </>
       }
       {all === 0 &&
