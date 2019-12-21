038467b2d6889bf9b266f0827b199b984be798cc
Henning Post <henningpost@gmail.com>
2019-12-20 16:35:05 -0800

    Fetch notes from json-server

--- part2/scratch/src/App.js
+++ part2/scratch/src/App.js
@@ -41,12 +41,15 @@ const App = () => {
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1
+      important: Math.random() > 0.5
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   return (
