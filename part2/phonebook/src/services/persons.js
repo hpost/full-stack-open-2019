62aceed6fa024b18ee7f6d89f6bc93a84fe548c5
Henning Post <henningpost@gmail.com>
2020-01-18 11:25:46 -0800

    Add Procfile to notes backend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://morning-reef-28818.herokuapp.com/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(response => response.data)
 
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
+app.use(cors())
 app.use(bodyParser.json())
 
 let notes = [{ id: 1, content: "HTML is easy", date: "2019-05-30T17:30:31.098Z", important: true }, { id: 2, content: "Browser can execute only Javascript", date: "2019-05-30T18:39:34.091Z", important: false }, { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2019-05-30T19:20:14.298Z", important: true }]
@@ -60,6 +62,7 @@ app.post('/notes', (req, res) => {
   res.json(note)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
